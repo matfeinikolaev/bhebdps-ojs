509004e507a01cbb05cac26a5495abfe85f4647f
matfeinikolaev <matfei.n@balcony.io>
2025-12-01 17:23:05 -0300

    задание 3.1 выполнено

--- Домашние задания/3.1 Убей кротов/base.js
+++ Домашние задания/3.1 Убей кротов/base.js
@@ -20,3 +20,28 @@
 
   next();
 })();
+
+let deadMoles = 0;
+let misses = 0;
+
+document.querySelectorAll('.hole').forEach(hole => {
+  hole.addEventListener('click', _ => {
+    if (hole.classList.contains('hole_has-mole')) {
+      deadMoles++;
+      if (deadMoles > 9) {
+        alert('Победа!');
+        deadMoles = 0;
+        misses = 0;
+      }
+    } else {
+      misses++;
+      if (misses > 4) {
+        alert('Вы проиграли!');
+        deadMoles = 0;
+        misses = 0;
+      }
+    }
+    document.querySelector('#dead').innerHTML = deadMoles;
+    document.querySelector('#lost').innerHTML = misses;
+  });
+});
