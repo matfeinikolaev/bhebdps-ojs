4773b5994e94ceaba860339cc67fcf503ee6038c
matfeinikolaev <matfei.n@balcony.io>
2025-12-06 09:16:37 -0300

    Задание 3.2 выполнено

---
+++ Домашние задания/3.2 Навигационное меню/task.js
@@ -0,0 +1,15 @@
+const links = document.querySelectorAll('.menu__link');
+
+links.forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        event.stopPropagation();
+        event.target.parentNode.childNodes.forEach(node => {
+            if (node.classList?.contains('menu_active')) {
+                node.classList.remove('menu_active');
+            } else if (node.classList?.contains('menu_sub')) {
+                node.classList.add('menu_active');
+            } 
+        });
+    })
+});
